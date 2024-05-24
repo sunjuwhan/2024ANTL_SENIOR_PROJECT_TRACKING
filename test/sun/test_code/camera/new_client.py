f0f7bff9bbbec4d35a20f3b32524990b796084a6
sunjuwhan <mathsun0011@gmail.com>
2024-05-24 13:15:25 +0900

    test

--- test/sun/test_code/camera/new_client.py
+++ test/sun/test_code/camera/new_client.py
@@ -2,10 +2,9 @@ import cv2
 import socket
 import numpy as np
 import threading
-import collections
-import struct
+
 # 클라이언트 IP와 포트 설정
-CLIENT_IP = '192.168.32.3'
+CLIENT_IP = '192.168.50.63'
 CLIENT_PORT = 9999
 ADDR = (CLIENT_IP, CLIENT_PORT)
 
@@ -13,31 +12,25 @@ ADDR = (CLIENT_IP, CLIENT_PORT)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.bind(ADDR)
 
-buffer = collections.defaultdict(lambda: [None] * 256)
-expected_frame_id = 0
+buffer = {}
 
 def receive_video():
-    global expected_frame_id
-
     while True:
         packet, _ = client_socket.recvfrom(65536)
-        packet_id, frame_id = struct.unpack("BH", packet[:3])
-        data = packet[3:]
+        packet_id = packet[0]
+        data = packet[1:]
         
-        if frame_id == expected_frame_id:
-            buffer[frame_id][packet_id] = data
-
-            if None not in buffer[frame_id]:
-                frame_data = b''.join(buffer[frame_id])
-                frame = np.frombuffer(frame_data, dtype=np.uint8)
-                frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-                if frame is not None:
-                    cv2.imshow('Received Video', frame)
-                
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-                del buffer[frame_id]
-                expected_frame_id = (expected_frame_id + 1) % 65536
+        buffer[packet_id] = data
+        
+        if packet_id == 0:
+            frame_data = b''.join([buffer[i] for i in sorted(buffer.keys())])
+            frame = np.frombuffer(frame_data, dtype=np.uint8)
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            cv2.imshow('Received Video', frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            buffer.clear()
 
 # 영상 수신 스레드 시작
 thread = threading.Thread(target=receive_video)
