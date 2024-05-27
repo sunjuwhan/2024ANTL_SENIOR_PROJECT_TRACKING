337a8fd4a66cc0345d85c48da39b17da411b49a9
sunjuwhan <mathsun0011@gmail.com>
2024-05-27 16:42:36 +0900

    Update main_process.py

--- test/sun/drone/main_process.py
+++ test/sun/drone/main_process.py
@@ -6,8 +6,10 @@ import os
 if __name__ == "__main__":
     with Pool(processes=2) as pool:  # 2개의 프로세스로 풀을 만듭니다.
         print("Start Multi Processing Drone")
-        result_A = pool.apply_async(MainCamera.run_camera_main)
-        result_B = pool.apply_async(MainDrone.run_drone_main)
+        main_camera=MainCamera()
+        main_drone=MainDrone()
+        result_A = pool.apply_async(main_camera.run_camera_main)
+        result_B = pool.apply_async(main_drone.run_drone_main)
         # 결과를 기다립니다.
         result_A.wait()
         result_B.wait()
