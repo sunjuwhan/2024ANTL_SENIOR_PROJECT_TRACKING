d07aca045e552b91ffac18b6989944c9ed5d178a
sunjuwhan <mathsun0011@gmail.com>
2024-05-27 15:22:45 +0900

    test

--- test/sun/drone/main.py
+++ test/sun/drone/main.py
@@ -7,13 +7,12 @@ from threading import *
 import time
 
 import view.socket_view_drone
+
 class Main():
     def __init__(self) -> None:
-
         self.__pilot_model=model.PilotModel()
         self.__gps_model=model.GpsModel()
         self.__controller=controller.master_drone_controller(self.__pilot_model,self.__gps_model)
-        
         self.__view= view.socket_view_drone(self.__pilot_model,self.__gps_model)
         
     def run(self):
@@ -27,7 +26,6 @@ class Main():
         #time.sleep(3)
         self.__view.run_drone()
         print("end camera & socket setting ")
-        
     async def run_pilot(self) :
         await self.__controller.run_pilot()
         
