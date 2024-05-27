c8b44729e45c754f83512bae6baf48e9d825fb34
sunjuwhan <mathsun0011@gmail.com>
2024-05-27 16:40:25 +0900

    Update main_camera.py

--- test/sun/drone/main_camera.py
+++ test/sun/drone/main_camera.py
@@ -6,17 +6,27 @@ from threading import *
 import time
 class MainCamera():
     def __init__(self) -> None:
-
+        print("1")
         self.__pilot_model=model.PilotModel()
+        print("2")
         self.__camera_model=model.VideoModel() 
+
+        print("3")
         self.__gps_model=model.GpsModel()
+        
+        print("4")
         self.__tracker_model=model.tracker_model()
-        
+
+        print("5")
         self.__controller=controller.Master_video_controller(self.__pilot_model,self.__camera_model,self.__gps_model,self.__tracker_model)
-        
+
+        print("6")
         self.__view= view.SocketView(self.__pilot_model,self.__camera_model,self.__gps_model)
         
+        print("7")
         self.__object=controller.ObjectController(self.__camera_model,self.__tracker_model)  
+        
+        print("8")
     def run(self):
         print("run object Detecter ")
         dectetor_thread=Thread(target=self.__object.run_object_detector) 
