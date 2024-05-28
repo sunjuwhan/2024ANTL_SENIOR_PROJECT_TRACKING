3ecf112f50a586047a23724f862a67c7bbaad8bc
sunjuwhan <114124984+sunjuwhan@users.noreply.github.com>
2024-05-28 19:59:59 +0900

    Update object_controller.py

--- test/sun/test_code/objectdetector/controller/object_controller.py
+++ test/sun/test_code/objectdetector/controller/object_controller.py
@@ -55,8 +55,8 @@ class ObjectController:
             # bbox된 이미지 데이터를 다시 카메라 프레임으로 설정
             bboxed_frame = self.image_manager.get_frame()
             
-            #cv2.imshow("test",cv2.resize(bboxed_frame,(640,480)))
-            #cv2.waitKey(0)
+            cv2.imshow("test",cv2.resize(bboxed_frame,(640,480)))
+            cv2.waitKey(0)
 
             #self.__video_model.set_frame2bboxed_frame(bboxed_frame)
         print(self.tool.get_count_person())
